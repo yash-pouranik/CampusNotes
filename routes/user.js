68422b1d235c85cf33f6eab08944165330738953
yash-pouranik <yashpouranik124@gmail.com>
2025-08-10 15:50:07 +0530

    Added - update profile

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const User = require("../models/user")
 const Note = require("../models/note")
+const {isLoggedIn} = require("../middlewares")
 
-router.get("/profile/:id", async (req, res) => {
+router.get("/profile/:id",  async (req, res) => {
   try {
     const user = await User.findById(req.params.id).populate('notes');
     if (!user) {
@@ -32,6 +33,42 @@ router.get("/rankings", async (req, res) => {
   }
 });
 
+
+router.get("/profile/:id/edit", isLoggedIn,  async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).populate('notes');
+    if (!user) {
+      return res.send("User does not exist");
+    }
+
+    res.render("user/editProfile", {
+      user,
+      title: user.username || user.name || "EDIT",
+    });
+  } catch (e) {
+    console.error(e);
+    res.send("Some error occurred", e);
+  }
+});
+
+
+router.put("/profile/:id/edit", isLoggedIn,  async (req, res) => {
+  try{
+    const {username, name, email, course} = req.body;
+    const user = await User.findByIdAndUpdate(req.params.id, {
+      username,
+      name,
+      email,
+      course,
+    });
+
+    req.flash("success", "Updated");
+    res.redirect(`/profile/${req.params.id}`)
+  }catch(e) {
+    req.flash("error", "Something went wrong at our end!");
+    res.redirect(`/profile/${req.params.id}`);
+  }
+})
 
 
 
