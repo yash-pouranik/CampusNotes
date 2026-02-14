34468c113785bb79bb5bd39b2d6e3713f5ab998b
yash-pouranik <yashpouranik124@gmail.com>
2026-02-14 23:13:36 +0530

    Fix upload loader & remove stitch folder

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,17 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuToggle = document.getElementById("menu-toggle");
   const navLinks = document.getElementById("nav-links");
 
-  menuToggle.addEventListener("click", (event) => {
-    event.stopPropagation();
-    navLinks.classList.toggle("active");
-  });
+  if (menuToggle && navLinks) {
+    menuToggle.addEventListener("click", (event) => {
+      event.stopPropagation();
+      navLinks.classList.toggle("active");
+    });
 
-  document.addEventListener("click", (event) => {
-    const isClickInsideMenu = navLinks.contains(event.target);
-    const isMenuOpen = navLinks.classList.contains("active");
+    document.addEventListener("click", (event) => {
+      const isClickInsideMenu = navLinks.contains(event.target);
+      const isMenuOpen = navLinks.classList.contains("active");
 
-    if (isMenuOpen && !isClickInsideMenu) {
-      navLinks.classList.remove("active");
-    }
-  });
+      if (isMenuOpen && !isClickInsideMenu) {
+        navLinks.classList.remove("active");
+      }
+    });
+  }
 });
