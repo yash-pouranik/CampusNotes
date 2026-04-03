ae4108240519c6281d79c334b5ede34e621272c8
yash-pouranik <yashpouranik124@gmail.com>
2026-04-04 00:51:49 +0530

    updated routes

--- routes/company.js
+++ routes/company.js
@@ -2,24 +2,6 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
-router.get("/bitbros/our-team", async (req, res) => {
-  try {
-    const teamMembers = await User.find({
-      $or: [
-        { "roles.isModerator": true },
-        { "roles.isDev": true }
-      ]
-    }).select("name username avatar roles socialLinks");
-
-    res.render("team/our-team", {
-      title: "Our Team | CampusNotes",
-      teamMembers
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error loading team");
-  }
-});
 
 router.get("/bitbros/aboutus", (req, res) => {
   res.render("team/aboutus", {title: "About Us | Campus Notes"});
